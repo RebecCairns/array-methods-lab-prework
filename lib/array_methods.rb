b8c1df6ba25183ec384ad5abde49ec928a4f1cb6
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-25 18:47:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,15 @@
 def using_include(array, element)
-
+  newArr = array.sort
+  num = array.length - 1
+  while(num >= 0)
+    if(newArr[num] == element)
+      returner = 'true'
+    else
+      returner = 'false'
+    end  
+    num-=1
+  end
+  return returner
 end
 
 def using_sort(array)
